348892db679b698bbf2d536170f81733ad0c769d
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-02-09 01:59:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    // origin:"http://localhost:5173",
     origin:"https://virtual-assistant-3-un4s.onrender.com",
     credentials:true
 }))
